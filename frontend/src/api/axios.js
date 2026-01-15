5fbe98c726827a6f369a2f172bd3c000f4bf2465
Sourav <145606223+sksrv@users.noreply.github.com>
2026-01-15 20:08:44 +0530

    Update API base URL in axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-krr3.onrender.com/",
+  baseURL: "https://expense-tracker-krr3.onrender.com/api",
 });
 
 // Attach token to every request
@@ -16,3 +16,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
